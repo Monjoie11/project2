42653aa4d207535597a868635d982171cca298e3
Dilshan <dilshan.diunugala@outlook.com>
2019-10-29 19:22:23 -0400

    p

--- com.revature.proj2/src/main/java/com/revature/project2/dao/PostDAO.java
+++ com.revature.proj2/src/main/java/com/revature/project2/dao/PostDAO.java
@@ -5,11 +5,11 @@ import java.util.List;
 import com.revature.project2.pojos.Post;
 
 public interface PostDAO {
-	public void createPost(Post p);
+	public void createPost(Post post);
 
-	public void deletePost(Post p);
+	public void deletePost(Post post);
 
-	public void updatePost(Post p);
+	public void updatePost(Post post);
 
 	public List<Post> getAllPosts();
 
