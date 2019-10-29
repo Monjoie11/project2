852460ded4f4c458bae65e04678393fddc2e3832
Shalom Nguyen <nguys2276@gmail.com>
2019-10-29 08:36:07 -0400

    UserDAO implemented

--- com.revature.proj2/src/main/java/dao/UserDAOImpl.java
+++ com.revature.proj2/src/main/java/dao/UserDAOImpl.java
@@ -2,37 +2,60 @@ package dao;
 
 import java.util.List;
 
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
 import com.revature.proj2.pojos.User;
+import com.revature.util.SessionFactoryUtil;
 
 public class UserDAOImpl implements UserDAO{
+	
+	SessionFactory sf = SessionFactoryUtil.getSessionFactory();
 
 	@Override
 	public User getUser(String email) {
-		// TODO Auto-generated method stub
-		return null;
+		Session sess = sf.openSession();
+		Transaction tx = sess.beginTransaction();
+		User user = (User) sess.get(User.class, email);
+		tx.commit();
+		sess.close();
+		return user;
 	}
 
 	@Override
 	public void createUser(User user) {
-		// TODO Auto-generated method stub
-		
+		Session sess = sf.openSession();
+		Transaction tx = sess.beginTransaction();
+		sess.save(user);
+		tx.commit();
+		sess.close();
 	}
 
 	@Override
 	public List<User> getAllUsers() {
-		// TODO Auto-generated method stub
-		return null;
+		Session sess = sf.openSession();
+		Criteria crit = sess.createCriteria(User.class);
+		List<User> result = crit.list();
+		sess.close();
+		return result;
 	}
 
 	@Override
 	public void updateUser(User user) {
-		// TODO Auto-generated method stub
-		
+		Session sess = sf.openSession();
+		sess.update(user);
+		sess.close();
 	}
 
 	@Override
 	public void deleteUser(User user) {
-		// TODO Auto-generated method stub
+		Session sess = sf.openSession();
+		Transaction tx = sess.beginTransaction();
+		sess.delete(user);
+		tx.commit();
+		sess.close();
 		
 	}
 
