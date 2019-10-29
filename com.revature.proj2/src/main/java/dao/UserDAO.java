73c7627b5efdd55d3ae3a8cd7de66786050ebd2f
Shalom Nguyen <nguys2276@gmail.com>
2019-10-28 22:56:53 -0400

    added UserDAO interface

--- com.revature.proj2/src/main/java/dao/UserDAO.java
+++ com.revature.proj2/src/main/java/dao/UserDAO.java
@@ -1,5 +1,15 @@
 package dao;
 
-public class UserDAO {
+import java.util.List;
 
+import com.revature.proj2.pojos.User;
+
+public interface UserDAO {
+	
+	public User getUser(String email);
+	public void createUser(User user);
+	public List<User> getAllUsers();
+	public void updateUser(User user);
+	public void deleteUser(User user);
+	
 }
---
+++ com.revature.proj2/src/main/java/dao/UserDAOImpl.java
@@ -0,0 +1,39 @@
+package dao;
+
+import java.util.List;
+
+import com.revature.proj2.pojos.User;
+
+public class UserDAOImpl implements UserDAO{
+
+	@Override
+	public User getUser(String email) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void createUser(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<User> getAllUsers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updateUser(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteUser(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
