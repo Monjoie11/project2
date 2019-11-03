9d62f2cbb46f4b4bffd2f15977762ff230bc4f0d
Shalom Nguyen <nguys2276@gmail.com>
2019-11-03 13:10:58 -0500

    Controller CrossOrigin to listen to requests

--- Proj2/src/main/java/com/revature/controller/LoginController.java
+++ Proj2/src/main/java/com/revature/controller/LoginController.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ import com.revature.pojo.User;
 import com.revature.service.AuthService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class LoginController {
 
 	private AuthService authService;
@@ -26,16 +28,17 @@ public class LoginController {
 	}
 
 	@PostMapping(consumes = "application/json", produces = "application/json", value = "/login")
-	public Object loginPost(@RequestBody String email, String password, BindingResult bindingResult, ModelMap modelMap, HttpSession sess) {
+	public Object loginPost(@RequestBody String email, String password, BindingResult bindingResult, ModelMap modelMap,
+			HttpSession sess) {
 
 		if (bindingResult.hasErrors()) {
 			modelMap.addAttribute("errorMessage", bindingResult.getAllErrors().get(0).getDefaultMessage());
 			error(bindingResult.getAllErrors().get(0).getDefaultMessage());
 			return null;
 		}
-		
+
 		Object authEntity = authService.validateEntity(email, password);
-		
+
 		if (authEntity != null && authEntity instanceof Company) {
 			sess.setAttribute("company", authEntity);
 			modelMap.addAttribute("company", authEntity);
@@ -45,7 +48,7 @@ public class LoginController {
 			modelMap.addAttribute("user", authEntity);
 			return authEntity;
 		}
-		
+
 		modelMap.addAttribute("errorMessage", "Username/Password did not match");
 
 		return authEntity;
