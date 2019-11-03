86701fc5fb4495e1f791c02d0e05724097da4287
Shalom Nguyen <nguys2276@gmail.com>
2019-11-03 01:10:24 -0400

    implemented gen/reset company access code

--- Proj2/src/main/java/com/revature/controller/CompanyController.java
+++ Proj2/src/main/java/com/revature/controller/CompanyController.java
@@ -1,6 +1,7 @@
 package com.revature.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,6 +28,16 @@ public class CompanyController {
 		companyService.registerCompany(company);
 		
 		return true;
+	}
+	
+	@GetMapping(produces = "application/json", value = "/company-access")
+	public String generateCompanyAccessCode(@RequestBody Company company) {
+		if(company == null) {
+			return null;
+		}
+		
+		return companyService.resetAccessCode(company);
+	
 	}
 
 }
--- Proj2/src/main/java/com/revature/service/CompanyService.java
+++ Proj2/src/main/java/com/revature/service/CompanyService.java
@@ -1,13 +1,13 @@
 package com.revature.service;
 
 import java.util.List;
+import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.revature.dao.CompanyDao;
 import com.revature.pojo.Company;
-import com.revature.pojo.User;
 
 @Service
 public class CompanyService {
@@ -29,6 +29,15 @@ public class CompanyService {
 	
 	public void registerCompany(Company company) {
 		companyDao.insertCompany(company);
+	}
+	
+	public String resetAccessCode(Company company) {
+		Random r = new Random();
+		int i = r.nextInt(999_999 + 1) + 100_000;
+		String accessCode = Integer.toString(i);
+		company.setAccessCode(accessCode);
+		companyDao.updateCompany(company);
+		return accessCode;
 	}
 
 }
