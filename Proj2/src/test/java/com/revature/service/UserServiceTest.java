c0ae69d440004ce18bca50c32bb8f3f1220baf30
Shalom Nguyen <nguys2276@gmail.com>
2019-11-07 19:44:36 -0500

    renamed UserService to UserServiceTest to avoid confusion with UserService

--- Proj2/src/test/java/com/revature/service/UserServiceTest.java
+++ Proj2/src/test/java/com/revature/service/UserServiceTest.java
@@ -8,7 +8,7 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class UserService {
+public class UserServiceTest {
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
