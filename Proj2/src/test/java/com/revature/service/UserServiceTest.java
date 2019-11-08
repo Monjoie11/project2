01e3b791c33d34042befad2d728a7909874adb43
Shalom Nguyen <nguys2276@gmail.com>
2019-11-07 22:55:51 -0500

    trying to use nick testing of service

--- Proj2/src/test/java/com/revature/service/UserServiceTest.java
+++ Proj2/src/test/java/com/revature/service/UserServiceTest.java
@@ -2,66 +2,75 @@ package com.revature.service;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
 
 import java.util.List;
+import java.util.Optional;
 
-import org.hibernate.HibernateException;
-import org.hibernate.SessionFactory;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Spy;
 import org.mockito.runners.MockitoJUnitRunner;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import com.revature.dao.UserDao;
+import com.revature.dao.UserDaoImpl;
 import com.revature.pojo.User;
-import com.revature.util.SessionFactoryUtil;
 
 @RunWith(MockitoJUnitRunner.class)
 public class UserServiceTest {
+    
+    @Mock
+    UserDaoImpl userDaoImpl;
+    
+    @InjectMocks
+    UserService userService = new UserService();
+    
+    User newUser;
+    
+    
+    /*
+     * @Spy HibernateException hibernateException = new
+     * HibernateException("Catch HibernateException");
+     */
+    /*
+     * @Mock SessionFactory sf = SessionFactoryUtil.getSessionFactory();
+     */
+    
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+    }
 
-	@Spy
-	UserService userService = new UserService();
-	
-	@Spy
-	HibernateException hibernateException = new HibernateException("Catch HibernateException");
-	
-	@Mock
-	SessionFactory sf = SessionFactoryUtil.getSessionFactory();
-	
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+    }
 
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
+    @Before
+    public void setUp() throws Exception {
+        newUser = new User("userTest@email.com", "testPassword");
+    }
 
-	@Before
-	public void setUp() throws Exception {
-	}
+    @After
+    public void tearDown() throws Exception {
+    }
 
-	@After
-	public void tearDown() throws Exception {
-	}
-
-	@Test
-	public void testGetUserByEmail() {
-		String email = "testinsertuser@junit.com";
-		String password = "passwordChanged";
-		User testUser = new User();
-		testUser.setEmail(email);
-		testUser.setPassword(password);
-		
-		User user = userService.getUserByEmail(email);
-		//assertEquals(testUser, user);
-		assertNotNull(user);
-	}
+    @Test
+    public void testGetUserByEmail() {
+        
+        when(userDaoImpl.getUserByEmail("userTest@email.com")).thenReturn(newUser);
+        assertEquals(Optional.of(newUser), userService.getUserByEmail("userTest@email.com"));
+        
+        /*
+         * String email = "testinsertuser@junit.com"; String password =
+         * "passwordChanged"; User testUser = new User(); testUser.setEmail(email);
+         * testUser.setPassword(password);
+         * 
+         * User user = userService.getUserByEmail(email); //assertEquals(testUser,
+         * user); assertNotNull(user);
+         */
+    }
 	
 	@Test
 	public void testGetAllUsers() {
