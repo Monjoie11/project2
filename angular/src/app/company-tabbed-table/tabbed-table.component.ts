6467433a43f406a7aa36913d2cf314cfc081f553
brian <brian.money92@yahoo.com>
2019-11-07 20:57:37 -0500

    Company homepage working

--- angular/src/app/company-tabbed-table/tabbed-table.component.ts
+++ angular/src/app/company-tabbed-table/tabbed-table.component.ts
@@ -19,22 +19,26 @@ export class TabbedTableComponent2 implements OnInit {
 
    }
 
-  ngOnInit() {
-    let obs = this.http.get('/company/posts');
-    obs.subscribe((response) => {
+  async ngOnInit() {
+    let obs = this.http.get('company/posts').toPromise();
+    await obs.then((response) => {
       this.response = response;
       var result = JSON.stringify(this.response);
       var result2 = JSON.parse(result);
+      console.log(result2);
       var rowCounter: number = 0;
 
       for (var key of Object.keys(result2)) {
-       //let model = {postId: result2['results'][rowCounter]['name']}
        let model;
-       if(result2['acceptingUser'] == null){
-        model = {postId: result2['postId'], postingUser: result2['postingUser']['email'], acceptingUser: result2['acceptingUser']['email'], shiftDate: result2['shiftDate'], startTime: result2['startTime'], endTime: result2['endTime']};
-       } else {
-        model = {postId: result2['postId'], postingUser: result2['postingUser']['email'], acceptingUser: result2['acceptingUser'], shiftDate: result2['shiftDate'], startTime: result2['startTime'], endTime: result2['endTime']};
-       }
+       if (result2[rowCounter]['acceptingUser'] == null && result2[rowCounter]['postingUser'] == null) {
+        model = { postId: result2[rowCounter]['postId'], postingUser: result2[rowCounter]['postingUser'], acceptingUser: result2[rowCounter]['acceptingUser'], shiftDate: result2[rowCounter]['shiftDate'], startTime: result2[rowCounter]['startTime'], endTime: result2[rowCounter]['endTime'] }
+      } else if(result2[rowCounter]['acceptingUser'] != null && result2[rowCounter]['postingUser'] == null) {
+        model = { postId: result2[rowCounter]['postId'], postingUser: result2[rowCounter]['postingUser'], acceptingUser: result2[rowCounter]['acceptingUser']['email'], shiftDate: result2[rowCounter]['shiftDate'], startTime: result2[rowCounter]['startTime'], endTime: result2[rowCounter]['endTime'] }
+      } else if(result2[rowCounter]['acceptingUser'] == null && result2[rowCounter]['postingUser'] != null) {
+        model = { postId: result2[rowCounter]['postId'], postingUser: result2[rowCounter]['postingUser']['email'], acceptingUser: result2[rowCounter]['acceptingUser'], shiftDate: result2[rowCounter]['shiftDate'], startTime: result2[rowCounter]['startTime'], endTime: result2[rowCounter]['endTime'] }
+      } else {
+        model = { postId: result2[rowCounter]['postId'], postingUser: result2[rowCounter]['postingUser']['email'], acceptingUser: result2[rowCounter]['acceptingUser']['email'], shiftDate: result2[rowCounter]['shiftDate'], startTime: result2[rowCounter]['startTime'], endTime: result2[rowCounter]['endTime'] }
+      }
        this.dataSource.push(model); 
        rowCounter++;
       }
@@ -42,54 +46,90 @@ export class TabbedTableComponent2 implements OnInit {
     });
   }
 
-  acceptPost(id){
-    let obs = this.http.put('/accept-company-acceptedpost/' + id, id);
-    obs.subscribe((response) => {
+  async acceptPost(id){
+    let obs = this.http.put('accept-company-acceptedpost/' + id, id).toPromise();
+    await obs.then((response) => {
       this.response = response;
       var result = JSON.stringify(this.response);
       var result2 = JSON.parse(result);
+      console.log(result2);
       if(result2 == true){
         alert("Post accepted")
       } else {
         alert("Error accepting post")
       }
-      this.dataSource = [...this.dataSource];
+      this.updateView();
     });
   }
 
-  denyPost(id){
-    let obs = this.http.put('/reject-company-acceptedpost/' + id, id);
-    obs.subscribe((response) => {
+  async denyPost(id){
+    let obs = this.http.put('reject-company-acceptedpost/' + id, id).toPromise();
+    await obs.then((response) => {
       this.response = response;
       var result = JSON.stringify(this.response);
       var result2 = JSON.parse(result);
+      console.log(result2);
       if(result2 == true){
         alert("Post denied")
       } else {
         alert("Error denying post")
       }
+      this.updateView();
+    });
+  }
+
+  async updateView() {
+    this.dataSource = [];
+    let obs = this.http.get('company/posts').toPromise();
+    await obs.then((response) => {
+      this.response = response;
+      var result = JSON.stringify(this.response);
+      var result2 = JSON.parse(result);
+      console.log(result2);
+      var rowCounter: number = 0;
+
+      for (var key of Object.keys(result2)) {
+        if (result2[rowCounter]['status'] == 'REPLIEDTO') {
+          let model;
+          if (result2[rowCounter]['acceptingUser'] == null && result2[rowCounter]['postingUser'] == null) {
+            model = { postId: result2[rowCounter]['postId'], postingUser: result2[rowCounter]['postingUser'], acceptingUser: result2[rowCounter]['acceptingUser'], shiftDate: result2[rowCounter]['shiftDate'], startTime: result2[rowCounter]['startTime'], endTime: result2[rowCounter]['endTime'], content: result2[rowCounter]['content'] }
+          } else if (result2[rowCounter]['acceptingUser'] != null && result2[rowCounter]['postingUser'] == null) {
+            model = { postId: result2[rowCounter]['postId'], postingUser: result2[rowCounter]['postingUser'], acceptingUser: result2[rowCounter]['acceptingUser']['email'], shiftDate: result2[rowCounter]['shiftDate'], startTime: result2[rowCounter]['startTime'], endTime: result2[rowCounter]['endTime'], content: result2[rowCounter]['content'] }
+          } else if (result2[rowCounter]['acceptingUser'] == null && result2[rowCounter]['postingUser'] != null) {
+            model = { postId: result2[rowCounter]['postId'], postingUser: result2[rowCounter]['postingUser']['email'], acceptingUser: result2[rowCounter]['acceptingUser'], shiftDate: result2[rowCounter]['shiftDate'], startTime: result2[rowCounter]['startTime'], endTime: result2[rowCounter]['endTime'], content: result2[rowCounter]['content'] }
+          } else {
+            model = { postId: result2[rowCounter]['postId'], postingUser: result2[rowCounter]['postingUser']['email'], acceptingUser: result2[rowCounter]['acceptingUser']['email'], shiftDate: result2[rowCounter]['shiftDate'], startTime: result2[rowCounter]['startTime'], endTime: result2[rowCounter]['endTime'], content: result2[rowCounter]['content'] }
+          }
+          this.dataSource.push(model);
+        }
+        rowCounter++;
+      }
       this.dataSource = [...this.dataSource];
     });
   }
 
-  yourFn($event){
+  async yourFn($event){
     this.dataSource = [];
     if($event.index === 0){
-      let obs = this.http.get('/company/posts');
-      obs.subscribe((response) => {
+      let obs = this.http.get('company/posts').toPromise();
+      await obs.then((response) => {
         this.response = response;
         var result = JSON.stringify(this.response);
         var result2 = JSON.parse(result);
+        console.log(result2);
         var rowCounter: number = 0;
   
         for (var key of Object.keys(result2)) {
-         //let model = {postId: result2['results'][rowCounter]['name']}
-         let model;
-         if(result2['acceptingUser'] == null){
-          model = {postId: result2['postId'], postingUser: result2['postingUser']['email'], acceptingUser: result2['acceptingUser']['email'], shiftDate: result2['shiftDate'], startTime: result2['startTime'], endTime: result2['endTime']};
-         } else {
-          model = {postId: result2['postId'], postingUser: result2['postingUser']['email'], acceptingUser: result2['acceptingUser'], shiftDate: result2['shiftDate'], startTime: result2['startTime'], endTime: result2['endTime']};
-         }
+          let model;
+          if (result2[rowCounter]['acceptingUser'] == null && result2[rowCounter]['postingUser'] == null) {
+            model = { postId: result2[rowCounter]['postId'], postingUser: result2[rowCounter]['postingUser'], acceptingUser: result2[rowCounter]['acceptingUser'], shiftDate: result2[rowCounter]['shiftDate'], startTime: result2[rowCounter]['startTime'], endTime: result2[rowCounter]['endTime'] }
+          } else if(result2[rowCounter]['acceptingUser'] != null && result2[rowCounter]['postingUser'] == null) {
+            model = { postId: result2[rowCounter]['postId'], postingUser: result2[rowCounter]['postingUser'], acceptingUser: result2[rowCounter]['acceptingUser']['email'], shiftDate: result2[rowCounter]['shiftDate'], startTime: result2[rowCounter]['startTime'], endTime: result2[rowCounter]['endTime'] }
+          } else if(result2[rowCounter]['acceptingUser'] == null && result2[rowCounter]['postingUser'] != null) {
+            model = { postId: result2[rowCounter]['postId'], postingUser: result2[rowCounter]['postingUser']['email'], acceptingUser: result2[rowCounter]['acceptingUser'], shiftDate: result2[rowCounter]['shiftDate'], startTime: result2[rowCounter]['startTime'], endTime: result2[rowCounter]['endTime'] }
+          } else {
+            model = { postId: result2[rowCounter]['postId'], postingUser: result2[rowCounter]['postingUser']['email'], acceptingUser: result2[rowCounter]['acceptingUser']['email'], shiftDate: result2[rowCounter]['shiftDate'], startTime: result2[rowCounter]['startTime'], endTime: result2[rowCounter]['endTime'] }
+          }
          this.dataSource.push(model); 
          rowCounter++;
         }
@@ -97,72 +137,84 @@ export class TabbedTableComponent2 implements OnInit {
       });
     } 
     if($event.index === 1){
-      let obs = this.http.get('/company/posts');
-      obs.subscribe((response) => {
+      let obs = this.http.get('company/posts').toPromise();
+      await obs.then((response) => {
         this.response = response;
         var result = JSON.stringify(this.response);
         var result2 = JSON.parse(result);
+        console.log(result2);
         var rowCounter: number = 0;
   
         for (var key of Object.keys(result2)) {
-         //let model = {postId: result2['results'][rowCounter]['name']}
-         let model;
-         if(result2['status'] === 'REPLIEDTO'){
-          if(result2['acceptingUser'] == null){
-            model = {postId: result2['postId'], postingUser: result2['postingUser']['email'], acceptingUser: result2['acceptingUser']['email'], shiftDate: result2['shiftDate'], startTime: result2['startTime'], endTime: result2['endTime']};
+         if(result2[rowCounter]['status'] == 'REPLIEDTO'){
+          let model;
+          if (result2[rowCounter]['acceptingUser'] == null && result2[rowCounter]['postingUser'] == null) {
+            model = { postId: result2[rowCounter]['postId'], postingUser: result2[rowCounter]['postingUser'], acceptingUser: result2[rowCounter]['acceptingUser'], shiftDate: result2[rowCounter]['shiftDate'], startTime: result2[rowCounter]['startTime'], endTime: result2[rowCounter]['endTime'] }
+          } else if(result2[rowCounter]['acceptingUser'] != null && result2[rowCounter]['postingUser'] == null) {
+            model = { postId: result2[rowCounter]['postId'], postingUser: result2[rowCounter]['postingUser'], acceptingUser: result2[rowCounter]['acceptingUser']['email'], shiftDate: result2[rowCounter]['shiftDate'], startTime: result2[rowCounter]['startTime'], endTime: result2[rowCounter]['endTime'] }
+          } else if(result2[rowCounter]['acceptingUser'] == null && result2[rowCounter]['postingUser'] != null) {
+            model = { postId: result2[rowCounter]['postId'], postingUser: result2[rowCounter]['postingUser']['email'], acceptingUser: result2[rowCounter]['acceptingUser'], shiftDate: result2[rowCounter]['shiftDate'], startTime: result2[rowCounter]['startTime'], endTime: result2[rowCounter]['endTime'] }
           } else {
-            model = {postId: result2['postId'], postingUser: result2['postingUser']['email'], acceptingUser: result2['acceptingUser'], shiftDate: result2['shiftDate'], startTime: result2['startTime'], endTime: result2['endTime']};
+            model = { postId: result2[rowCounter]['postId'], postingUser: result2[rowCounter]['postingUser']['email'], acceptingUser: result2[rowCounter]['acceptingUser']['email'], shiftDate: result2[rowCounter]['shiftDate'], startTime: result2[rowCounter]['startTime'], endTime: result2[rowCounter]['endTime'] }
           }
+          this.dataSource.push(model); 
         }
-         this.dataSource.push(model); 
          rowCounter++;
         }
         this.dataSource = [...this.dataSource];
       });
     } 
     if($event.index === 2){
-      let obs = this.http.get('/company/posts');
+      let obs = this.http.get('company/posts');
       obs.subscribe((response) => {
         this.response = response;
         var result = JSON.stringify(this.response);
         var result2 = JSON.parse(result);
+        console.log(result2);
         var rowCounter: number = 0;
   
         for (var key of Object.keys(result2)) {
-         //let model = {postId: result2['results'][rowCounter]['name']}
-         let model;
-         if(result2['status'] === 'ACCEPTED'){
-          if(result2['acceptingUser'] == null){
-            model = {postId: result2['postId'], postingUser: result2['postingUser']['email'], acceptingUser: result2['acceptingUser']['email'], shiftDate: result2['shiftDate'], startTime: result2['startTime'], endTime: result2['endTime']};
+         if(result2[rowCounter]['status'] == 'ACCEPTED'){
+          let model;
+          if (result2[rowCounter]['acceptingUser'] == null && result2[rowCounter]['postingUser'] == null) {
+            model = { postId: result2[rowCounter]['postId'], postingUser: result2[rowCounter]['postingUser'], acceptingUser: result2[rowCounter]['acceptingUser'], shiftDate: result2[rowCounter]['shiftDate'], startTime: result2[rowCounter]['startTime'], endTime: result2[rowCounter]['endTime'] }
+          } else if(result2[rowCounter]['acceptingUser'] != null && result2[rowCounter]['postingUser'] == null) {
+            model = { postId: result2[rowCounter]['postId'], postingUser: result2[rowCounter]['postingUser'], acceptingUser: result2[rowCounter]['acceptingUser']['email'], shiftDate: result2[rowCounter]['shiftDate'], startTime: result2[rowCounter]['startTime'], endTime: result2[rowCounter]['endTime'] }
+          } else if(result2[rowCounter]['acceptingUser'] == null && result2[rowCounter]['postingUser'] != null) {
+            model = { postId: result2[rowCounter]['postId'], postingUser: result2[rowCounter]['postingUser']['email'], acceptingUser: result2[rowCounter]['acceptingUser'], shiftDate: result2[rowCounter]['shiftDate'], startTime: result2[rowCounter]['startTime'], endTime: result2[rowCounter]['endTime'] }
           } else {
-            model = {postId: result2['postId'], postingUser: result2['postingUser']['email'], acceptingUser: result2['acceptingUser'], shiftDate: result2['shiftDate'], startTime: result2['startTime'], endTime: result2['endTime']};
+            model = { postId: result2[rowCounter]['postId'], postingUser: result2[rowCounter]['postingUser']['email'], acceptingUser: result2[rowCounter]['acceptingUser']['email'], shiftDate: result2[rowCounter]['shiftDate'], startTime: result2[rowCounter]['startTime'], endTime: result2[rowCounter]['endTime'] }
           }
+          this.dataSource.push(model); 
         }
-         this.dataSource.push(model); 
          rowCounter++;
         }
         this.dataSource = [...this.dataSource];
       });
     } 
     if($event.index === 3){
-      let obs = this.http.get('/company/posts');
+      let obs = this.http.get('company/posts');
       obs.subscribe((response) => {
         this.response = response;
         var result = JSON.stringify(this.response);
         var result2 = JSON.parse(result);
+        console.log(result2);
         var rowCounter: number = 0;
   
         for (var key of Object.keys(result2)) {
-         //let model = {postId: result2['results'][rowCounter]['name']}
-         let model;
-         if(result2['status'] === 'REJECTED'){
-          if(result2['acceptingUser'] == null){
-            model = {postId: result2['postId'], postingUser: result2['postingUser']['email'], acceptingUser: result2['acceptingUser']['email'], shiftDate: result2['shiftDate'], startTime: result2['startTime'], endTime: result2['endTime']};
+         if(result2[rowCounter]['status'] == 'COMPLETED'){
+          let model;
+          if (result2[rowCounter]['acceptingUser'] == null && result2[rowCounter]['postingUser'] == null) {
+            model = { postId: result2[rowCounter]['postId'], postingUser: result2[rowCounter]['postingUser'], acceptingUser: result2[rowCounter]['acceptingUser'], shiftDate: result2[rowCounter]['shiftDate'], startTime: result2[rowCounter]['startTime'], endTime: result2[rowCounter]['endTime'] }
+          } else if(result2[rowCounter]['acceptingUser'] != null && result2[rowCounter]['postingUser'] == null) {
+            model = { postId: result2[rowCounter]['postId'], postingUser: result2[rowCounter]['postingUser'], acceptingUser: result2[rowCounter]['acceptingUser']['email'], shiftDate: result2[rowCounter]['shiftDate'], startTime: result2[rowCounter]['startTime'], endTime: result2[rowCounter]['endTime'] }
+          } else if(result2[rowCounter]['acceptingUser'] == null && result2[rowCounter]['postingUser'] != null) {
+            model = { postId: result2[rowCounter]['postId'], postingUser: result2[rowCounter]['postingUser']['email'], acceptingUser: result2[rowCounter]['acceptingUser'], shiftDate: result2[rowCounter]['shiftDate'], startTime: result2[rowCounter]['startTime'], endTime: result2[rowCounter]['endTime'] }
           } else {
-            model = {postId: result2['postId'], postingUser: result2['postingUser']['email'], acceptingUser: result2['acceptingUser'], shiftDate: result2['shiftDate'], startTime: result2['startTime'], endTime: result2['endTime']};
+            model = { postId: result2[rowCounter]['postId'], postingUser: result2[rowCounter]['postingUser']['email'], acceptingUser: result2[rowCounter]['acceptingUser']['email'], shiftDate: result2[rowCounter]['shiftDate'], startTime: result2[rowCounter]['startTime'], endTime: result2[rowCounter]['endTime'] }
           }
+          this.dataSource.push(model); 
         }
-         this.dataSource.push(model); 
          rowCounter++;
         }
         this.dataSource = [...this.dataSource];
